ddcae4bc07242ea1caaf8d5c08992a963458222b
Guga Villar <grsv.21@gmail.com>
2025-01-14 19:06:30 -0300

    :sparkles: Add sound

--- src/components/Atoms/CurrentCode/CurrentCode.tsx
+++ src/components/Atoms/CurrentCode/CurrentCode.tsx
@@ -1,20 +1,41 @@
 'use client'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
+import useSound from 'use-sound'
+
 import { useCode } from '@/context/useCode'
 import { lastCalled } from '@/helpers/findLastCalled'
+import { twMerge } from 'tailwind-merge'
 
 export const CurrentCode = () => {
+	const [bounce, setBounce] = useState(false)
+
 	const { codes } = useCode()
+	const [play] = useSound('/assets/tone.mp3')
 	const lastCalledCode = lastCalled(codes)
+
 	const activeCode = lastCalledCode?.number
 		? String(lastCalledCode?.number).padStart(3, '0')
 		: ''
+
+	useEffect(() => {
+		if (!lastCalledCode?.called_at) return
+
+		play()
+		setBounce(true)
+
+		setTimeout(() => setBounce(false), 1500)
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [lastCalledCode?.called_at])
 
 	return (
 		<div className="row-span-2 flex h-full flex-col items-center justify-center gap-6 bg-brand p-4 text-white">
 			<Image width={300} height={300} src="/assets/logo.png" alt="logo" />
 			<h2 className="text-4xl font-semibold">Senha</h2>
-			<h1 className="text-9xl font-bold">{activeCode}</h1>
+			<h1 className={twMerge('text-9xl font-bold', bounce && 'animate-bounce')}>
+				{activeCode}
+			</h1>
 			{lastCalledCode?.guiche && (
 				<h3 className="text-6xl font-semibold">
 					Guichê {lastCalledCode?.guiche}
